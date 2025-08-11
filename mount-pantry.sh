de80e49860b52630591ece0f15fedf424737d3ba
Andy Jackson <andrew.jackson@dpconline.org>
2025-08-11 14:11:25 +0000

    rclone mount working on Codespaces.

--- mount-pantry.sh
+++ mount-pantry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Experimenting with mounting remote corpora, doesn't seem to work
-mkdir ~/ipres-pantry
-rclone mount pantry: ~/ipres-pantry 2>&1 > rclone-mount.log &
+# Experimenting with mounting remote corpora, doesn't seem to work without raised container priviledges:
+mkdir -p ./ipres-pantry
+rclone mount --vfs-cache-mode full pantry: ./ipres-pantry 2>&1 > rclone-mount.log &
 
